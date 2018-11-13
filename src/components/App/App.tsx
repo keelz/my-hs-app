d49b4051c2108b610e3217fd10ba59e7a5fbb6fb
Lloyd Brigs <lloydb1@gmail.com>
2018-11-13 08:38:59 -0800

    src directory structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import logo from '../../common/assets/img/logo.svg';
+import './App.css';
+
+const App: React.SFC = () =>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.tsx</code> and save to reload.
+    </p>
+  </div>;
+
+export default App;
---
+++ src/components/App/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import App from './App';
+
+const Component: React.SFC = () => <App />;
+
+export default Component;
