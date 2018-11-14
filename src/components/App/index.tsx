bf61ac7a6a7682000f85c5efe1008c5995c3a621
Lloyd Brigs <lloydb1@gmail.com>
2018-11-13 23:49:35 -0800

    app index comments

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import App from './App';
 
+// implementation props
 type AppProps = {};
 
+// state props
 type AppStateProps = {
   loaded: boolean;
   appDidLoadAction: () => any;
 };
 
+// composed props
 type Props = AppProps & AppStateProps;
 
 class Index extends React.Component<Props> {
