7293905d52355962e5fd38e9c10582538b082aae
Lloyd Brigs <lloydb1@gmail.com>
2018-11-14 09:18:04 -0800

    css naming

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 const LoadingAnimation: React.SFC = () =>
   <div className="loading-animation-container">
-    <div className="loading-animation" />
+    <div className="loading-animation-spinner" />
   </div>;
 
 export default LoadingAnimation;
