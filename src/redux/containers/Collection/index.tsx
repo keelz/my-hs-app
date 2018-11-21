66cbb607544d762a18722c654ec3bf31a255f612
Lloyd Brigs <lloydb1@gmail.com>
2018-11-20 23:08:59 -0800

    small fix

--- src/redux/containers/Collection/index.tsx
+++ src/redux/containers/Collection/index.tsx
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 import { IRootState } from '../../Types';
 import Collection, { ICollectionStateProps } from '../../../components/Collection';
+import { CardClassName } from '../../../common/models/Card';
 import {
   selectActiveCardClassName,
   selectCardsForActiveClassName
 } from '../../selectors/cards';
 
 const mapStateToProps = (state: IRootState): ICollectionStateProps => ({
-  activeCardClassName: selectActiveCardClassName(state),
+  activeCardClassName: selectActiveCardClassName(state) as CardClassName,
   collection: {
     cards: selectCardsForActiveClassName(state),
   },
