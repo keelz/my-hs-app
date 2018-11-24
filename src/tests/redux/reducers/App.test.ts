78e3b5e03761dedb3cece4f7c079a98647fd29f6
Lloyd Brigs <lloydb1@gmail.com>
2018-11-24 12:38:52 -0800

    app reducer test

--- src/tests/redux/reducers/App.test.ts
+++ src/tests/redux/reducers/App.test.ts
@@ -1,5 +1,6 @@
 import reducer, {
   appDidLoadAction,
+  appSetLoadedAction,
   appStateFactory,
   APP_DID_LOAD,
   APP_SET_LOADED,
@@ -16,34 +17,49 @@ const compose = () => {
 };
 
 describe('App reducer', () => {
-  it('composes default state correctly', () => {
-    const state = appStateFactory();
-    expect(state).toMatchSnapshot();
-  });
-
-  it(`dispatchs ${APP_DID_LOAD} correctly`, () => {
-    const { store } = compose();
-    appDidLoadAction()(store.dispatch);
-    expect(store.dispatch).toHaveBeenCalledWith({
-      type: APP_DID_LOAD,
+  describe('factories', () => {
+    it('composes default state correctly', () => {
+      const state = appStateFactory();
+      expect(state).toMatchSnapshot();
     });
   });
 
-  it(`handles ${APP_DID_LOAD} correctly`, () => {
-    const state = appStateFactory();
-    const newState = reducer(state, { type: APP_DID_LOAD });
-    expect(newState).toEqual(state);
+  describe('action creators', () => {
+    it(`dispatchs ${APP_DID_LOAD} correctly`, () => {
+      const { store } = compose();
+      appDidLoadAction()(store.dispatch);
+      expect(store.dispatch).toHaveBeenCalledWith({
+        type: APP_DID_LOAD,
+      });
+    });
+
+    it(`dispatches ${APP_SET_LOADED} correctly`, () => {
+      const { store } = compose();
+      appSetLoadedAction(true)(store.dispatch);
+      expect(store.dispatch).toHaveBeenCalledWith({
+        type: APP_SET_LOADED,
+        payload: { loaded: true },
+      });
+    });
   });
 
-  it(`handles ${APP_SET_LOADED} correctly`, () => {
-    const state = appStateFactory();
-    const newState = reducer(state, {
-      type: APP_SET_LOADED,
-      payload: { loaded: true },
+  describe('reducer', () => {
+    it(`handles ${APP_DID_LOAD} correctly`, () => {
+      const state = appStateFactory();
+      const newState = reducer(state, { type: APP_DID_LOAD });
+      expect(newState).toEqual(state);
     });
-    expect(newState).toEqual({
-      ...state,
-      loaded: true,
+
+    it(`handles ${APP_SET_LOADED} correctly`, () => {
+      const state = appStateFactory();
+      const newState = reducer(state, {
+        type: APP_SET_LOADED,
+        payload: { loaded: true },
+      });
+      expect(newState).toEqual({
+        ...state,
+        loaded: true,
+      });
     });
   });
 });
