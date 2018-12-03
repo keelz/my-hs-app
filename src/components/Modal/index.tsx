c123c5b53efe487f4ca19015c61903bae2f0e2db
Lloyd Brigs <lloydb1@gmail.com>
2018-12-03 01:28:20 -0800

    card details modal progress

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as classnames from 'classnames';
 import Modal from '@material-ui/core/Modal';
 import { IComponentProps } from '../../common/models/app.model';
+import { composeClassname } from '../../common/utils';
 
 interface ISimpleModalProps extends IComponentProps {
   open: boolean;
@@ -13,9 +14,14 @@ const SimpleModal: React.SFC<ISimpleModalProps> = props =>
   <Modal
     aria-labelledby="simple-modal-title"
     aria-describedby="simple-modal-description"
+    className="flex items-center justify-center"
     open={props.open}
     onClose={props.onCloseCallback}>
-    <div className={classnames(props.className)}>
+    <div
+      className={composeClassname(classnames([
+        'App-modal-container',
+        'flex',
+        'items-center']))(props.className)}>
       {props.render && props.render()}
     </div>
   </Modal>;
