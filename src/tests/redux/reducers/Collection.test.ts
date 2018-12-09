99b54164e99ebab68b0b73ad7ec1b349c0eb2e7f
Lloyd Brigs <lloydb1@gmail.com>
2018-12-09 11:07:54 -0800

    test organization

--- src/tests/redux/reducers/Collection.test.ts
+++ src/tests/redux/reducers/Collection.test.ts
@@ -5,38 +5,44 @@ import reducer, {
 } from '../../../redux/reducers/Collection';
 import { ModalState } from '../../../redux/Types';
 
-describe('Collection Reducer', () => {
-  it('composes state with factory correctly', () => {
-    const state = collectionStateFactory();
-    expect(state).toMatchSnapshot();
-  });
-
-  it(`dispatches ${COLLECTION_SET_MODAL} correctly`, () => {
-    const dispatch = jest.fn();
-    setCollectionModalAction(ModalState.OPEN)(dispatch);
-    expect(dispatch).toHaveBeenCalledWith({
-      type: COLLECTION_SET_MODAL,
-      payload: { modal: ModalState.OPEN },
+describe('Collection', () => {
+  describe('factories', () => {
+    it('composes state with factory correctly', () => {
+      const state = collectionStateFactory();
+      expect(state).toMatchSnapshot();
     });
   });
 
-  it('composes default state correctly', () => {
-    const action = {
-      type: 'UNKNOWN',
-    };
-    const state = reducer(undefined, action);
-    expect(state).toEqual(collectionStateFactory());
+  describe('action creators', () => {
+    it(`dispatches ${COLLECTION_SET_MODAL} correctly`, () => {
+      const dispatch = jest.fn();
+      setCollectionModalAction(ModalState.OPEN)(dispatch);
+      expect(dispatch).toHaveBeenCalledWith({
+        type: COLLECTION_SET_MODAL,
+        payload: { modal: ModalState.OPEN },
+      });
+    });
   });
 
-  it(`composes state for ${COLLECTION_SET_MODAL} correctly`, () => {
-    const action = {
-      type: COLLECTION_SET_MODAL,
-      payload: { modal: ModalState.OPEN },
-    };
-    const state = reducer(collectionStateFactory(), action);
-    expect(state).toEqual({
-      ...collectionStateFactory(),
-      modal: ModalState.OPEN,
+  describe('reducer', () => {
+    it('composes default state correctly', () => {
+      const action = {
+        type: 'UNKNOWN',
+      };
+      const state = reducer(undefined, action);
+      expect(state).toEqual(collectionStateFactory());
+    });
+
+    it(`composes state for ${COLLECTION_SET_MODAL} correctly`, () => {
+      const action = {
+        type: COLLECTION_SET_MODAL,
+        payload: { modal: ModalState.OPEN },
+      };
+      const state = reducer(collectionStateFactory(), action);
+      expect(state).toEqual({
+        ...collectionStateFactory(),
+        modal: ModalState.OPEN,
+      });
     });
   });
 });
