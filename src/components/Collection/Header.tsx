e3e195e5adbe1af35417993a1400b777859fdc18
Lloyd Brigs <lloydb1@gmail.com>
2018-12-09 23:49:11 -0800

    play style button and state

--- src/components/Collection/Header.tsx
+++ src/components/Collection/Header.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import { IComponentProps } from '../../common/models/App.model';
 import { composeClassname } from '../../common/utils';
 import ManaBar from '../../redux/containers/Collection/ManaBar';
+import PlayStyle from '../../redux/containers/Collection/PlayStyle';
 
 interface ICollectionHeaderProps extends IComponentProps {}
 
 const CollectionHeader: React.SFC<ICollectionHeaderProps> = props =>
   <div className={composeClassname('Collection-header')(props.className)}>
     <ManaBar />
+    <PlayStyle />
   </div>;
 
 export default CollectionHeader;
---
+++ src/components/Collection/PlayStyle.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { IComponentProps } from '../../common/models/App.model';
+import { composeClassname } from '../../common/utils';
+
+const wildIcon = require('../../common/assets/img/99px-wild_icon_large.png');
+const standardIcon = require('../../common/assets/img/100px-standard_icon_large.png');
+
+interface IPlayStyleProps extends IComponentProps {}
+
+interface IPlayStyleStateProps {
+  filters: { [field: string]: string };
+  setFilter: (name: string, value: string) => any;
+}
+
+type Props = IPlayStyleProps & IPlayStyleStateProps;
+
+const PlayStyle: React.SFC<Props> = props =>
+  <div className={composeClassname('Collection-play-style')(props.className)}>
+    { props.filters['PLAY_STYLE'] === 'WILD'
+      ? <img
+        src={wildIcon}
+        alt="wild-ico"
+        onClick={() => props.setFilter('PLAY_STYLE', 'STANDARD')}
+        title="Filter Standard Cards" />
+      : <img
+        src={standardIcon}
+        alt="standard-ico"
+        onClick={() => props.setFilter('PLAY_STYLE', 'WILD')}
+        title="Filter Wild Cards" />
+    }
+  </div>;
+
+export default PlayStyle;
